51afed7d51b9e341b9f4ba813410bd1ffb59bc59
Guillaume Kingham <guil.king@t-online.de>
2021-07-29 11:56:07 +0200

    Changed Comment to Ilyas sucks moorreeee
    :)

--- UnityGOB/Assets/_Scripts/Main.cs
+++ UnityGOB/Assets/_Scripts/Main.cs
@@ -7,7 +7,7 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Gulliaume Sucks");
+        Debug.Log("Ilyas sucks mmooooooooooooooooooooore");
     }
 
     // Update is called once per frame
